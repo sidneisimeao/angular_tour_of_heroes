074bb916e68b38ba3168214285c7719e7c637acc
Sidnei Simeão <sidneisimmon@gmail.com>
2019-10-22 13:16:12 -0300

    Tutorial 5

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,19 +1,25 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 
-//import 'src/hero_service.dart';
-import 'src/hero_list_component.dart';
+import 'src/routes.dart';
+import 'src/hero_service.dart';
 
 @Component(
   selector: 'my-app',
   template: '''
-    <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+     <h1>{{title}}</h1>
+ <nav>
+    <a [routerLink]="RoutePaths.dashboard.toUrl()"
+       [routerLinkActive]="'active'">Dashboard</a>
+    <a [routerLink]="RoutePaths.heroes.toUrl()"
+       [routerLinkActive]="'active'">Heroes</a>
+  </nav>
+  <router-outlet [routes]="Routes.all"></router-outlet>
   ''',
-  directives: [HeroListComponent],
-  //providers: [ClassProvider(HeroService)]
+  directives: [routerDirectives],
+  providers: [ClassProvider(HeroService)],
+  exports: [RoutePaths, Routes]
 )
-
-class AppComponent{
+class AppComponent {
   final title = 'Tour of Heroes';
 }
--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -2,6 +2,7 @@ import 'package:angular_router/angular_router.dart';
 
 import 'route_paths.dart';
 import 'hero_list_component.template.dart' as hero_list_template;
+import 'dashborad_component.template.dart' as dashborad_template;
 
 export 'route_paths.dart';
 
@@ -11,7 +12,18 @@ class Routes {
     component: hero_list_template.HeroListComponentNgFactory,
   );
 
+  static final dashborad = RouteDefinition(
+    routePath: RoutePaths.dashboard,
+    component: dashborad_template.DashboardComponentNgFactory,
+    // useAsDefault: true,
+  );
+
   static final all = <RouteDefinition>[
     heroes,
+    dashborad,
+    RouteDefinition.redirect(
+        path: '',
+        redirectTo: RoutePaths.dashboard
+            .toUrl()) //  useAsDefault: true, substitui o redirecionamento
   ];
 }
